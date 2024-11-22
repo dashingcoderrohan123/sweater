a37971226616f8595254c3c35dab34d8a5a20d3d
dashingcoderrohan123 <anandrao23046@gmail.com>
2024-11-22 07:31:29 -0800

    Add files via upload

---
+++ lesson7.py
@@ -0,0 +1,10 @@
+temperature=int(input("enter the temperature"))
+
+if temperature < 10:
+    print("It is very cold wear a jacket")
+
+elif 10 <= temperature <= 20:
+    print("it is little cold wear light layers of fleece")
+
+else:
+    print(" The weather is mild you can wear light and soft clothes")
